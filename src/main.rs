34c6388059700a2e7ac45d88e44313cc43672cc7
Endre Márk Borza <endremborza@gmail.com>
2024-06-20 10:46:00 +0200

    align main

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod common;
 mod ingest_entity;
 mod quercus;
 //spec
+mod aggregate_quercus;
 mod oa_csv_writers;
 mod oa_entity_mapping;
 mod oa_filters;
@@ -12,7 +13,9 @@ mod oa_fix_atts;
 mod oa_structs;
 mod oa_var_atts;
 mod prune_quercus;
+mod quercus_packet;
 
+use aggregate_quercus::aggregate;
 use common::Stowage;
 use oa_csv_writers::write_csvs;
 use oa_entity_mapping::make_ids;
@@ -21,6 +24,7 @@ use oa_fix_atts::write_fix_atts;
 use oa_var_atts::write_var_atts;
 use prune_quercus::prune;
 use quercus::dump_all_cache;
+use quercus_packet::dump_packets;
 
 fn main() -> io::Result<()> {
     let mut args = std::env::args();
@@ -48,6 +52,10 @@ fn main() -> io::Result<()> {
             dump_all_cache(stowage)?;
         } else if comm == "prune-qcs" {
             prune(stowage)?;
+        } else if comm == "agg-qcs" {
+            aggregate(stowage)?;
+        } else if comm == "packet-qcs" {
+            dump_packets(stowage)?;
         }
     }
     Ok(())
