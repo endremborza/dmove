33f8ff7d6e8ee19e257c8422bf487e53ceb6f194
Endre Márk Borza <endremborza@gmail.com>
2025-01-24 11:35:51 +0100

    rl server tinkers

--- rankless_server/src/main.rs
+++ rankless_server/src/main.rs
@@ -1,24 +1,26 @@
+#![feature(future_join)]
 use axum::{
+    body::Body,
     extract::{Path, Query, State},
-    http::Method,
+    http::{header::CACHE_CONTROL, HeaderMap, HeaderValue, Method},
+    response::{IntoResponse, Response},
     routing::get,
     Json, Router,
 };
 use axum_server::tls_rustls::RustlsConfig;
-use core::f64;
-use dmove::{
-    ByteArrayInterface, CompactEntity, Entity, MarkedAttribute, NamespacedEntity, UnsignedNumber,
-    VariableSizeAttribute,
-};
+use dmove::{Entity, UnsignedNumber};
 use hashbrown::HashMap;
 use kd_tree::{KdPoint, KdTree};
 use rand::{seq::SliceRandom, Rng};
 use serde::{Deserialize, Serialize};
 use std::{
     cmp::{max, min},
+    future::join,
     net::SocketAddr,
+    ops::Deref,
     sync::Arc,
 };
+
 use tower::ServiceBuilder;
 use tower_http::{
     compression::CompressionLayer,
@@ -28,44 +30,57 @@ use tower_http::{
 
 use muwo_search::SearchEngine;
 use rankless_rs::{
-    gen_entity_mapping::{Authors, Countries, Institutions, Sources, Subfields},
-    CiteCountMarker, NameExtensionMarker, NameMarker, QuickestBox, ReadIter, SemanticIdMarker,
-    Stowage, WorkCountMarker,
+    gen::a1_entity_mapping::{Authors, Countries, Institutions, Qs, Sources, Subfields, Topics},
+    Stowage,
 };
+use rankless_trees::{
+    instances::TreeGetter,
+    interfacing::{Getters, NodeInterfaces, RootInterfaceable, RootInterfaces},
+    io::{TreeQ, TreeResponse, TreeSpecMap, TreeSpecs},
+    AttributeLabelUnion,
+};
+
+type SemanticIdMap = HashMap<String, usize>;
 
 #[derive(Deserialize)]
 struct BasicQ {
     q: Option<String>,
 }
 
-#[derive(Serialize, Clone)]
+#[derive(Serialize)]
 struct ViewResult {
     name: String,
     citations: usize,
+    #[serde(rename = "dmId")]
+    dm_id: usize,
     papers: usize,
     similars: Vec<SearchResult>,
 }
 
-#[derive(Serialize, Clone)]
+#[derive(Serialize)]
 struct TopResult {
     name: String,
     entities: Vec<SearchResult>,
 }
 
-#[derive(Serialize, Clone)]
+#[derive(Serialize)]
 struct EntityDescription {
     name: String,
     count: usize,
 }
 
-#[derive(Clone)]
 struct NameState {
-    engine: Arc<SearchEngine>,
-    responses: Arc<[SearchResult]>,
-    means: Arc<[f64; 2]>,
-    vars: Arc<[f64; 2]>,
-    semantic_id_map: Arc<HashMap<String, usize>>,
-    query_tree: Arc<KdTree<KDItem>>,
+    engine: SearchEngine,
+    responses: Box<[SearchResult]>,
+    means: Box<[f64; 2]>,
+    vars: Box<[f64; 2]>,
+    semantic_id_map: SemanticIdMap,
+    query_tree: KdTree<KDItem>,
+}
+
+struct TreeBasisState {
+    gets: Arc<Getters>,
+    att_union: Arc<AttributeLabelUnion>,
 }
 
 #[derive(Serialize, Clone)]
@@ -75,6 +90,8 @@ struct SearchResult {
     semantic_id: String,
     #[serde(skip_serializing)]
     full_name: String,
+    #[serde(skip_serializing)]
+    dm_id: usize,
     papers: u32,
     citations: u32,
 }
@@ -113,46 +130,24 @@ impl KdPoint for KDItem {
 }
 
 impl NameState {
-    fn new<E>(stowage: &Stowage) -> Self
+    fn new<E>(entif: &RootInterfaces<E>) -> Self
     where
-        E: PrepFilter
-            + MarkedAttribute<NameMarker>
-            + MarkedAttribute<WorkCountMarker>
-            + MarkedAttribute<CiteCountMarker>
-            + MarkedAttribute<NameExtensionMarker>
-            + MarkedAttribute<SemanticIdMarker>,
-        <E as MarkedAttribute<NameMarker>>::AttributeEntity:
-            NamespacedEntity + Entity<T = String> + VariableSizeAttribute + CompactEntity,
-
-        <E as MarkedAttribute<SemanticIdMarker>>::AttributeEntity:
-            NamespacedEntity + Entity<T = String> + VariableSizeAttribute + CompactEntity,
-
-        <E as MarkedAttribute<NameExtensionMarker>>::AttributeEntity:
-            NamespacedEntity + Entity<T = String> + VariableSizeAttribute + CompactEntity,
-
-        <E as MarkedAttribute<WorkCountMarker>>::AttributeEntity: NamespacedEntity + CompactEntity,
-        <<E as MarkedAttribute<WorkCountMarker>>::AttributeEntity as Entity>::T:
-            ByteArrayInterface + UnsignedNumber,
-        <E as MarkedAttribute<CiteCountMarker>>::AttributeEntity: NamespacedEntity + CompactEntity,
-        <<E as MarkedAttribute<CiteCountMarker>>::AttributeEntity as Entity>::T:
-            ByteArrayInterface + UnsignedNumber,
+        E: RootInterfaceable + PrepFilter,
     {
-        let anames = stowage.get_marked_interface::<E, NameMarker, ReadIter>();
-        let name_exts = stowage.get_marked_interface::<E, NameExtensionMarker, ReadIter>();
-        let sem_ids = stowage.get_marked_interface::<E, SemanticIdMarker, ReadIter>();
-        let wcounts = stowage.get_marked_interface::<E, WorkCountMarker, QuickestBox>();
-        let ccounts = stowage.get_marked_interface::<E, CiteCountMarker, QuickestBox>();
-
-        let mut responses: Vec<SearchResult> = anames
-            .zip(name_exts)
-            .zip(sem_ids)
+        let mut responses: Vec<SearchResult> = entif
+            .names
+            .0
+            .iter()
+            .zip(entif.name_exts.0.iter())
+            .zip(entif.sem_ids.0.iter())
             .enumerate()
             .map(|(i, ((name, ext), semantic_id))| SearchResult {
                 full_name: format!("{} {}", name, ext).trim().to_string(),
-                name,
-                semantic_id,
-                papers: wcounts[i].to_usize() as u32,
-                citations: ccounts[i].to_usize() as u32,
+                name: name.to_string(),
+                semantic_id: semantic_id.to_string(),
+                papers: entif.wcounts[i].to_usize() as u32,
+                citations: entif.ccounts[i].to_usize() as u32,
+                dm_id: i,
             })
             .filter(|sr| E::filter_sr(sr))
             .collect();
@@ -205,32 +200,54 @@ impl NameState {
 }
 
 macro_rules! multi_route {
-    ($s: ident, $($T: ty),*) => {
+    ($g: ident, $($T: ty),*) => {
         {
+            let ccount = $g.total_cite_count();
             let mut v = Vec::new();
             let mut tops = Vec::new();
+            let mut specs: TreeSpecMap = HashMap::new();
+            let mut static_att_union: AttributeLabelUnion = HashMap::new();
+            NodeInterfaces::<Topics>::new(&$g.stowage).update_stats(&mut static_att_union, ccount);
+            NodeInterfaces::<Qs>::new(&$g.stowage).update_stats(&mut static_att_union, ccount);
             $(v.push(
-                EntityDescription {
-                    name: <$T as Entity>::NAME.to_string(),
-                    count:<$T as Entity>::N}
+                    EntityDescription {
+                        name: <$T as Entity>::NAME.to_string(),
+                        count:<$T as Entity>::N
+                    }
                 );
             )*
-            (Router::new()
+
+            let name_state_route = Router::new()
             $(.route(&format!("/names/{}", <$T as Entity>::NAME), get(name_get))
                 .route(&format!("/slice/{}/:from/:to", <$T as Entity>::NAME), get(slice_get))
                 .route(&format!("/views/{}/:semantic_id", <$T as Entity>::NAME), get(view_get))
                 .with_state({
-                    let nstate = NameState::new::<$T>(&$s);
+                    specs.insert(<$T as Entity>::NAME.to_string(), <$T as TreeGetter>::get_specs());
+                    let ent_intf = RootInterfaces::<$T>::new(&$g.stowage);
+                    let nstate = NameState::new::<$T>(&ent_intf);
+                    ent_intf.update_stats(&mut static_att_union, ccount);
                     let entities = top_slice(&nstate.responses);
                     tops.push(
                         TopResult {name: <$T as Entity>::NAME.to_string(), entities }
                     );
-                    nstate
+                    nstate.into()
                 })
-            )*,
-            v,
-            tops
-            )
+            )*;
+
+            let att_union = Arc::new(static_att_union);
+            let tree_route = Router::new()
+            $(
+                .route(&format!("/{}", <$T as Entity>::NAME), get(tree_get::<$T>))
+                .with_state({
+                    let ts = TreeBasisState {
+                        gets: $g.clone(),
+                        att_union: att_union.clone(),
+                    };
+                    ts.into()
+                })
+            )*;
+
+            (name_state_route, tree_route, v, tops, specs)
         }
     };
 }
@@ -238,8 +255,8 @@ macro_rules! multi_route {
 #[tokio::main]
 async fn main() {
     let path: String = std::env::args().last().unwrap();
-    println!("read from path: {}", path);
-    let stowage = Stowage::new(&path);
+    let now = std::time::Instant::now();
+    println!("reading from path: {}", path);
     let cors = CorsLayer::new()
         .allow_methods([Method::GET, Method::POST])
         .allow_origin(Any);
@@ -248,24 +265,22 @@ async fn main() {
         .gzip(true)
         .quality(CompressionLevel::Fastest);
 
-    let (response_api, entity_descriptions, tops) = multi_route!(
-        stowage,
-        Authors,
-        Institutions,
-        Sources,
-        Subfields,
-        Countries
-    );
+    let gets = Arc::new(Getters::new(Stowage::new(&path)));
+    let (response_api, tree_api, entity_descriptions, tops, specs) =
+        multi_route!(gets, Authors, Institutions, Sources, Subfields, Countries);
 
-    let count_api = Router::new()
-        .route("/", get(count_get))
-        .with_state(entity_descriptions);
+    let count_api = static_router(&entity_descriptions);
+    let specs_api = static_router(&TreeSpecs::new(specs));
 
-    let tops_api = Router::new().route("/", get(tops_get)).with_state(tops);
+    let tops_api = Router::new()
+        .route("/", get(tops_get))
+        .with_state(Arc::new(tops));
 
     let api = Router::new()
         .nest("/", response_api)
+        .nest("/trees", tree_api)
         .nest("/counts", count_api)
+        .nest("/specs", specs_api)
         .nest("/tops", tops_api)
         .layer(ServiceBuilder::new().layer(cors).layer(compression));
 
@@ -283,36 +298,51 @@ async fn main() {
 
     let loc_addr = SocketAddr::from(([127, 0, 0, 1], 3038));
     let rem_addr = SocketAddr::from(([0, 0, 0, 0], 3039));
+    println!("loaded and set-up in {}", now.elapsed().as_secs());
     println!("listening on local: {}; remote: {}", loc_addr, rem_addr);
-    axum_server::bind(loc_addr)
-        .serve(app.clone().into_make_service())
-        .await
-        .unwrap();
-    axum_server::bind_rustls(rem_addr, config)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    let a1 = axum_server::bind(loc_addr).serve(app.clone().into_make_service());
+    let a2 = axum_server::bind(SocketAddr::from(([127, 0, 0, 1], 3040)))
+        .serve(app.clone().into_make_service());
+    let a3 = axum_server::bind_rustls(rem_addr, config).serve(app.into_make_service());
+    let (r1, r2, r3) = join!(a1, a2, a3).await;
+    r1.unwrap();
+    r2.unwrap();
+    r3.unwrap();
 }
 
 async fn slice_get(
     Path(ends): Path<(usize, usize)>,
-    state: State<NameState>,
-) -> Json<Vec<SearchResult>> {
+    state: State<Arc<NameState>>,
+) -> Response<Body> {
     const MAX_SLICE: usize = 1000;
     let start = min(ends.0, state.responses.len() - 1);
     let end = min(
         max(start + 1, min(start + MAX_SLICE, ends.1)),
         state.responses.len(),
     );
-    let out = state.responses[start..end].to_vec();
-    Json(out)
+    Json(&state.responses[start..end]).into_response()
 }
 
-async fn count_get(count_state: State<Vec<EntityDescription>>) -> Json<Vec<EntityDescription>> {
-    Json(count_state.to_vec())
+async fn state_get(str_state: State<Arc<str>>) -> (HeaderMap, Response<Body>) {
+    (cache_header(60), str_state.to_string().into_response())
 }
 
-async fn tops_get(tops_state: State<Vec<TopResult>>) -> Json<Vec<TopResult>> {
+async fn tree_get<E>(
+    tree_q: Query<TreeQ>,
+    state: State<Arc<TreeBasisState>>,
+) -> (HeaderMap, Json<Option<TreeResponse>>)
+where
+    E: Entity + TreeGetter,
+{
+    let resp = Json(E::get_tree(
+        &state.gets,
+        &state.att_union,
+        tree_q.deref().to_owned(),
+    ));
+    (cache_header(60), resp)
+}
+
+async fn tops_get(tops_state: State<Arc<Vec<TopResult>>>) -> Json<Vec<TopResult>> {
     let mut rng = rand::thread_rng();
     const TOP_N: usize = 5;
     let out = tops_state
@@ -331,11 +361,15 @@ async fn tops_get(tops_state: State<Vec<TopResult>>) -> Json<Vec<TopResult>> {
     Json(out)
 }
 
-async fn view_get(Path(semantic_id): Path<String>, state: State<NameState>) -> Json<ViewResult> {
+async fn view_get(
+    Path(semantic_id): Path<String>,
+    state: State<Arc<NameState>>,
+) -> Json<ViewResult> {
     let default = ViewResult {
         name: "-".to_string(),
         citations: 0,
         papers: 0,
+        dm_id: 0,
         similars: Vec::new(),
     };
     let iopt = state.semantic_id_map.get(&semantic_id);
@@ -355,6 +389,7 @@ async fn view_get(Path(semantic_id): Path<String>, state: State<NameState>) -> J
                 .collect();
             ViewResult {
                 name: srs.name.clone(),
+                dm_id: srs.dm_id.clone(),
                 citations: srs.citations as usize,
                 papers: srs.papers as usize,
                 similars,
@@ -364,16 +399,29 @@ async fn view_get(Path(semantic_id): Path<String>, state: State<NameState>) -> J
     Json(out)
 }
 
-async fn name_get(q: Query<BasicQ>, state: State<NameState>) -> Json<Vec<SearchResult>> {
+async fn name_get(
+    q: Query<BasicQ>,
+    state: State<Arc<NameState>>,
+) -> (HeaderMap, Json<Vec<SearchResult>>) {
     let q_string = q.q.clone().unwrap();
     let top_n_inds = state.engine.query(&q_string);
-    Json(
+    let resp = Json(
         top_n_inds
             .into_iter()
             .filter(|e| (*e as usize) < state.responses.len())
             .map(|e| state.responses[e as usize].clone())
             .collect(),
-    )
+    );
+    (cache_header(60), resp)
+}
+
+fn cache_header(mins: usize) -> HeaderMap {
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        CACHE_CONTROL,
+        HeaderValue::from_str(&format!("public, max-age={}", mins * 60)).unwrap(),
+    );
+    headers
 }
 
 fn get_arr_base(res: &SearchResult) -> [f64; 2] {
@@ -383,7 +431,7 @@ fn get_arr_base(res: &SearchResult) -> [f64; 2] {
     ]
 }
 
-fn get_query_arr(res: &SearchResult, state: &State<NameState>) -> [f64; 2] {
+fn get_query_arr(res: &SearchResult, state: &State<Arc<NameState>>) -> [f64; 2] {
     let mut rec = get_arr_base(res);
     for i in 0..rec.len() {
         rec[i] -= state.means[i];
@@ -392,8 +440,13 @@ fn get_query_arr(res: &SearchResult, state: &State<NameState>) -> [f64; 2] {
     rec
 }
 
-fn top_slice<T: Clone>(v: &Arc<[T]>) -> Vec<T> {
+fn top_slice<T: Clone>(v: &Box<[T]>) -> Vec<T> {
     let ve = max(max(200, v.len() / 10), 10000);
     let end = min(ve, v.len());
     return v[..end].to_vec();
+}
+
+fn static_router<O: Serialize>(o: &O) -> Router {
+    let arc: Arc<str> = Arc::from(serde_json::to_string(o).unwrap().as_str());
+    Router::new().route("/", get(state_get)).with_state(arc)
 }
