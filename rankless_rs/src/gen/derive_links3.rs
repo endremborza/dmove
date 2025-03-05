4d2349126272abaef90315cedcec0863faf725c0
Endre Márk Borza <endremborza@gmail.com>
2025-03-05 17:59:53 +0100

    run new steps

--- rankless_rs/src/gen/derive_links3.rs
+++ rankless_rs/src/gen/derive_links3.rs
@@ -14,60 +14,60 @@ pub struct HitPapers { }
 
 pub struct CountryWorks { }
 
-impl Entity for SourcesWorkCount { type T = u32; const N: usize =  37187; const NAME: & str =  "sources-work-count"; }
+impl Entity for SourcesWorkCount { type T = u32; const N: usize = 37187; const NAME: & str = "sources-work-count"; }
 
 impl MappableEntity for SourcesWorkCount { type KeyType = usize; }
 
-impl NamespacedEntity for SourcesWorkCount { const NS: & str =  "derive_links3"; }
+impl NamespacedEntity for SourcesWorkCount { const NS: & str = "derive_links3"; }
 
 impl MarkedAttribute<crate::common::WorkCountMarker> for crate::gen::a1_entity_mapping::Sources { type AttributeEntity = SourcesWorkCount; }
 
-impl Entity for InstitutionsWorkCount { type T = u32; const N: usize =  19833; const NAME: & str =  "institutions-work-count"; }
+impl Entity for InstitutionsWorkCount { type T = u32; const N: usize = 19833; const NAME: & str = "institutions-work-count"; }
 
 impl MappableEntity for InstitutionsWorkCount { type KeyType = usize; }
 
-impl NamespacedEntity for InstitutionsWorkCount { const NS: & str =  "derive_links3"; }
+impl NamespacedEntity for InstitutionsWorkCount { const NS: & str = "derive_links3"; }
 
 impl MarkedAttribute<crate::common::WorkCountMarker> for crate::gen::a1_entity_mapping::Institutions { type AttributeEntity = InstitutionsWorkCount; }
 
-impl Entity for AuthorsWorkCount { type T = u32; const N: usize =  4070683; const NAME: & str =  "authors-work-count"; }
+impl Entity for AuthorsWorkCount { type T = u16; const N: usize = 4088379; const NAME: & str = "authors-work-count"; }
 
 impl MappableEntity for AuthorsWorkCount { type KeyType = usize; }
 
-impl NamespacedEntity for AuthorsWorkCount { const NS: & str =  "derive_links3"; }
+impl NamespacedEntity for AuthorsWorkCount { const NS: & str = "derive_links3"; }
 
 impl MarkedAttribute<crate::common::WorkCountMarker> for crate::gen::a1_entity_mapping::Authors { type AttributeEntity = AuthorsWorkCount; }
 
-impl Entity for SubfieldsWorkCount { type T = u32; const N: usize =  254; const NAME: & str =  "subfields-work-count"; }
+impl Entity for SubfieldsWorkCount { type T = u32; const N: usize = 254; const NAME: & str = "subfields-work-count"; }
 
 impl MappableEntity for SubfieldsWorkCount { type KeyType = usize; }
 
-impl NamespacedEntity for SubfieldsWorkCount { const NS: & str =  "derive_links3"; }
+impl NamespacedEntity for SubfieldsWorkCount { const NS: & str = "derive_links3"; }
 
 impl MarkedAttribute<crate::common::WorkCountMarker> for crate::gen::a1_entity_mapping::Subfields { type AttributeEntity = SubfieldsWorkCount; }
 
-impl Entity for TopicsWorkCount { type T = u32; const N: usize =  4518; const NAME: & str =  "topics-work-count"; }
+impl Entity for TopicsWorkCount { type T = u32; const N: usize = 4518; const NAME: & str = "topics-work-count"; }
 
 impl MappableEntity for TopicsWorkCount { type KeyType = usize; }
 
-impl NamespacedEntity for TopicsWorkCount { const NS: & str =  "derive_links3"; }
+impl NamespacedEntity for TopicsWorkCount { const NS: & str = "derive_links3"; }
 
 impl MarkedAttribute<crate::common::WorkCountMarker> for crate::gen::a1_entity_mapping::Topics { type AttributeEntity = TopicsWorkCount; }
 
-impl Entity for CountryWorks { type T = Box<[u32]>; const N: usize =  230; const NAME: & str =  "country-works"; }
+impl Entity for CountryWorks { type T = Box<[u32]>; const N: usize = 230; const NAME: & str = "country-works"; }
 
 impl MappableEntity for CountryWorks { type KeyType = usize; }
 
 impl VariableSizeAttribute for CountryWorks { type SizeType = u32; }
 
-impl NamespacedEntity for CountryWorks { const NS: & str =  "derive_links3"; }
+impl NamespacedEntity for CountryWorks { const NS: & str = "derive_links3"; }
 
 impl Link for CountryWorks { type Source = crate::gen::a1_entity_mapping::Countries; type Target = crate::gen::a1_entity_mapping::Works; }
 
 impl MarkedAttribute<crate::common::MainWorkMarker> for crate::gen::a1_entity_mapping::Countries { type AttributeEntity = CountryWorks; }
 
-impl Entity for HitPapers { type T = u32; const N: usize =  4498420; const NAME: & str =  "hit-papers"; }
+impl Entity for HitPapers { type T = u32; const N: usize = 4507617; const NAME: & str = "hit-papers"; }
 
 impl MappableEntity for HitPapers { type KeyType = u64; }
 
-impl NamespacedEntity for HitPapers { const NS: & str =  "derive_links3"; }
+impl NamespacedEntity for HitPapers { const NS: & str = "derive_links3"; }
